0d9d933ff5caf84644ca1528df073dff149a50d5
Sun <38485966+LeSun54@users.noreply.github.com>
2018-11-02 11:54:30 +0800

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,10 @@
+Title     : Loops
+Subdomain : Introduction
+Domain    : Python
+Author    : Leon Sun
+Created   : 02 Nov 2018
+
+if __name__ == '__main__':
+    n = int(input())
+for i in range(0, n):
+    print (i*i)
